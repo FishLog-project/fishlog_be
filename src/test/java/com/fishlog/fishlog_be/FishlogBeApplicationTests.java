22f94721216a6ad6886a23ea64e1f18f1c1ebe03
s2000ten <s2000ten@naver.com>
2026-07-06 16:31:02 +0900

    ìinfra setting prepare project

---
+++ src/test/java/com/fishlog/fishlog_be/FishlogBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fishlog.fishlog_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FishlogBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
