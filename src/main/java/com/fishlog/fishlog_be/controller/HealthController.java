66cf4aa20664645bb63ebd875fcb36cc434a75d1
s2000ten <s2000ten@naver.com>
2026-07-08 10:40:03 +0900

    fix: 오타 수정2

--- src/main/java/com/fishlog/fishlog_be/controller/HealthController.java
+++ src/main/java/com/fishlog/fishlog_be/controller/HealthController.java
@@ -13,6 +13,6 @@ public class HealthController {
 
     @GetMapping("/health")
     public Map<String, String> health() {
-        return Map.of("ci cd status", "ok");
+        return Map.of("status", "ok");
     }
 }
