ddb559cc204acec99574aa36d28003ec1e4593af
imjuyongp <imjuyongp@gmail.com>
2026-07-09 14:16:50 +0900

    :recycle:refactor: spotless apply

--- src/main/java/com/fishlog/fishlog_be/FishlogBeApplication.java
+++ src/main/java/com/fishlog/fishlog_be/FishlogBeApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FishlogBeApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(FishlogBeApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(FishlogBeApplication.class, args);
+  }
 }
